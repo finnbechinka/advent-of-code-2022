7f6e64e0562d012a38109ffd66f4b78eeea947de
Finn <contact@bechinka.eu>
2022-12-01 21:57:07 +0100

    chore: add gitignore; remove already commited files

--- day-1/target/debug/deps/day_1-312cef370508a3e9.d
+++
@@ -1,5 +0,0 @@
-c:\Users\finn-\Documents\code\advent-of-code-2022\day-1\target\debug\deps\day_1-312cef370508a3e9.rmeta: src\main.rs
-
-c:\Users\finn-\Documents\code\advent-of-code-2022\day-1\target\debug\deps\day_1-312cef370508a3e9.d: src\main.rs
-
-src\main.rs:
--- day-1/target/debug/deps/day_1-8efdf646851635ca.d
+++
@@ -1,5 +0,0 @@
-c:\Users\finn-\Documents\code\advent-of-code-2022\day-1\target\debug\deps\day_1-8efdf646851635ca.rmeta: src\main.rs
-
-c:\Users\finn-\Documents\code\advent-of-code-2022\day-1\target\debug\deps\day_1-8efdf646851635ca.d: src\main.rs
-
-src\main.rs:
